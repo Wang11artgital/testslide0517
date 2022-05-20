2de2ffcb93b0bb9285e0ea87ad3088fe0ef2c66e
Wang11artgital <104808648+Wang11artgital@users.noreply.github.com>
2022-05-20 12:15:04 +0800

    up

--- js/game_icecream.js
+++ js/game_icecream.js
@@ -4,7 +4,7 @@ export default class Game{
         this.app = new PIXI.Application({
             width: window.innerWidth,
             height: window.innerHeight,
-            backgroundColor: 0xffffff,
+            backgroundAlpha:0.7,
             view: this.canvas
         })
         this.startBgTextureArray = []
@@ -43,23 +43,25 @@ export default class Game{
                 this.gameset.loader.add(`startBg-${i}`,`images/${i}.jpg`)
             }
             this.gameset.loader
-                .add('bg','image/bg.jpg')
-                .add('sprite','image/sprite.png')
-                .add('heart','image/sprite2.png')
+                .add('bg','image/Rectangle.png')
+                .add('sprite','image/ice2.png')
+                .add('heart','image/ice.png')
                 .add('mask','image/mask.png')
-                .add('back','image/back.png')
+                .add('exit','image/exit.png')
                 .add('continue','image/continue.png')
                 .add('rulebg','image/rulebg.png')
                 .add('startbtn','image/startbtn.png')
+                .add('nice','image/nice.png')
                 .load((loader, resource)=>{
                     this.gameset.textures.bg = new PIXI.Texture(resource.bg.texture)
                     this.gameset.textures.sprite = new PIXI.Texture(resource.sprite.texture)
                     this.gameset.textures.heart = new PIXI.Texture(resource.heart.texture)
                     this.gameset.textures.mask = new PIXI.Texture(resource.mask.texture)
-                    this.gameset.textures.back = new PIXI.Texture(resource.back.texture)
+                    this.gameset.textures.exit = new PIXI.Texture(resource.exit.texture)
                     this.gameset.textures.continue = new PIXI.Texture(resource.continue.texture)
                     this.gameset.textures.rulebg = new PIXI.Texture(resource.rulebg.texture)
                     this.gameset.textures.startbtn = new PIXI.Texture(resource.startbtn.texture)
+                    this.gameset.textures.nice = new PIXI.Texture(resource.nice.texture)
                     for(let i = 1; i < 11; i++){
                         let texture = new PIXI.Texture(resource[`startBg-${i}`].texture);
                         this.startBgTextureArray.push(texture);
@@ -78,7 +80,6 @@ export default class Game{
         this.gameset.container.allStage.addChild(this.gameset.container.gameStage);
         this.canvas.classList.remove("loadering");
         this.gameset.hearts = 3;
-        this.app.renderer.backgroundColor = 0xfaa0aa;
         this.resizeCanvas();
         
         // 底層遊戲-背景
@@ -89,38 +90,42 @@ export default class Game{
         this.gameset.sprites.bg.height = this.gameset.size.h;
         this.gameset.sprites.bg.anchor.set(0.5);
         this.gameset.container.gameStage.addChild(this.gameset.sprites.bg);
-        // 底層遊戲-中間精靈
-        this.gameset.sprites.sprite = new PIXI.Sprite(this.gameset.textures.sprite);
-        this.gameset.sprites.sprite.anchor.set(0.5);
-        this.gameset.sprites.sprite.x = this.gameset.size.w / 2;
-        this.gameset.sprites.sprite.y = this.gameset.size.h / 2;
-        this.gameset.sprites.sprite.width = this.gameset.size.w*0.2;
-        this.gameset.sprites.sprite.height = this.gameset.size.w*0.2;
+        // 底層遊戲-能量條
+        this.gameset.container.allStage.addChild(this.gameset.container.bar);
+        this.gameset.sprites.innerBar = new PIXI.Graphics();
+        this.gameset.sprites.innerBar.beginFill(0x650a30);
+        this.gameset.sprites.innerBar.drawRect(0, 0, this.gameset.size.w*0.9, 4);
+        this.gameset.sprites.innerBar.endFill();
+        this.gameset.container.bar.addChild(this.gameset.sprites.innerBar);
+        this.gameset.sprites.pointBar = new PIXI.Graphics();
+        this.gameset.sprites.pointBar.beginFill(0x650a30);
+        this.gameset.sprites.pointBar.drawRect(0, -4, this.gameset.size.w*0.3, 12);
+        this.gameset.sprites.pointBar.endFill();
+        this.gameset.sprites.pointBar.x = Math.floor(Math.random()*this.gameset.size.w*0.6);
+        this.gameset.container.bar.addChild(this.gameset.sprites.pointBar);
+        this.gameset.sprites.nice = new PIXI.Sprite(this.gameset.textures.nice);
+        this.gameset.sprites.nice.anchor.set(0.5);
+        this.gameset.sprites.nice.scale.set(this.gameset.size.w*0.12/65)
+        this.gameset.sprites.nice.x = this.gameset.sprites.pointBar.x + this.gameset.sprites.pointBar.width/2;
+        this.gameset.sprites.nice.y = -30;
+        this.gameset.container.bar.addChild(this.gameset.sprites.nice);
+        this.gameset.sprites.outerBar = new PIXI.Sprite(this.gameset.textures.sprite);
+        this.gameset.sprites.outerBar.anchor.set(0.5);
+        this.gameset.sprites.outerBar.width = this.gameset.size.w*0.05;
+        this.gameset.sprites.outerBar.height = this.gameset.size.w*0.05;
+        this.gameset.container.bar.addChild(this.gameset.sprites.outerBar);
+        this.gameset.sprites.outerBar.x = Math.floor(Math.random()*this.gameset.size.w*0.7);
+        this.gameset.sprites.outerBar.vx = 5;
+        this.gameset.container.bar.position.set(this.gameset.size.w / 2-this.gameset.container.bar.width/2, this.gameset.size.h*0.9);
+        this.gameset.container.gameStage.addChild(this.gameset.container.heartArea);
+        // 點擊區
+        this.gameset.sprites.sprite = new PIXI.Sprite;
+        this.gameset.sprites.sprite.width = this.gameset.size.w;
+        this.gameset.sprites.sprite.height = this.gameset.size.h;
         this.gameset.sprites.sprite.interactive = false;
         this.gameset.sprites.sprite.buttonMode = true;
         this.gameset.sprites.sprite.on('pointerdown', this.shootBar.bind(this));
         this.gameset.container.gameStage.addChild(this.gameset.sprites.sprite);
-        // 底層遊戲-能量條
-        this.gameset.container.allStage.addChild(this.gameset.container.bar);
-        this.gameset.sprites.innerBar = new PIXI.Graphics();
-        this.gameset.sprites.innerBar.beginFill(0x000000);
-        this.gameset.sprites.innerBar.drawRect(0, 0, 228, 16);
-        this.gameset.sprites.innerBar.endFill();
-        this.gameset.container.bar.addChild(this.gameset.sprites.innerBar);
-        this.gameset.sprites.pointBar = new PIXI.Graphics();
-        this.gameset.sprites.pointBar.beginFill(0xFFB200);
-        this.gameset.sprites.pointBar.drawRect(60, 0, 50, 16);
-        this.gameset.sprites.pointBar.endFill();
-        this.gameset.container.bar.addChild(this.gameset.sprites.pointBar);
-        this.gameset.sprites.outerBar = new PIXI.Graphics();
-        this.gameset.sprites.outerBar.beginFill(0xFF3300);
-        this.gameset.sprites.outerBar.drawRect(0, 0, 228, 16);
-        this.gameset.sprites.outerBar.endFill();
-        this.gameset.container.bar.addChild(this.gameset.sprites.outerBar);
-        this.gameset.sprites.outerBar.width = 28;
-        this.gameset.sprites.outerBar.x = Math.floor(Math.random()*200);
-        this.gameset.sprites.outerBar.vx = 4;
-        this.gameset.container.bar.position.set(this.gameset.size.w / 2-this.gameset.container.bar.width/2, this.gameset.size.h*0.9);
         // 中層規則-背景
         this.gameset.container.allStage.addChild(this.gameset.container.ruleStage);
         this.gameset.sprites.ruleBg = new PIXI.Sprite(this.gameset.textures.rulebg);
@@ -152,7 +157,7 @@ export default class Game{
         this.gameset.sprites.continuebtn = new PIXI.Sprite(this.gameset.textures.continue);
         this.gameset.sprites.continuebtn.anchor.set(0.5);
         this.gameset.sprites.continuebtn.x = this.gameset.size.w / 2;
-        this.gameset.sprites.continuebtn.y = this.gameset.size.h*0.25;
+        this.gameset.sprites.continuebtn.y = this.gameset.size.h*0.5;
         this.gameset.sprites.continuebtn.interactive = true;
         this.gameset.sprites.continuebtn.buttonMode = true;
         this.gameset.sprites.continuebtn.on('pointerdown', this.continueGame.bind(this));
@@ -161,19 +166,10 @@ export default class Game{
         // 上層結果頁-背景
         this.gameset.container.allStage.addChild(this.gameset.container.resultStage);
         this.gameset.sprites.resultBg = new PIXI.Graphics();
-        this.gameset.sprites.resultBg.beginFill(0xffffff);
+        this.gameset.sprites.resultBg.beginFill(0x000000);
         this.gameset.sprites.resultBg.drawRect(0, 0, this.gameset.size.w, this.gameset.size.h);
         this.gameset.sprites.resultBg.endFill();
         this.gameset.container.resultStage.addChild(this.gameset.sprites.resultBg);
-        // 上層結果頁-按鈕
-        this.gameset.sprites.back = new PIXI.Sprite(this.gameset.textures.back);
-        this.gameset.sprites.back.anchor.set(0.5);
-        this.gameset.sprites.back.x = this.gameset.size.w*0.9;
-        this.gameset.sprites.back.y = this.gameset.size.h*0.1;
-        this.gameset.sprites.back.interactive = true;
-        this.gameset.sprites.back.buttonMode = true;
-        this.gameset.sprites.back.on('pointerdown', this.closeCanvas.bind(this));
-        this.gameset.container.resultStage.addChild(this.gameset.sprites.back);
         this.gameset.container.resultStage.visible = false;
         // 上層開始頁-背景
         this.gameset.container.allStage.addChild(this.gameset.container.startStage);
@@ -223,7 +219,16 @@ export default class Game{
             .on('pointerupoutside', this.onDragEnd)
             .on('pointermove', this.onDragMove.bind(this));
         this.gameset.container.startStage.visible = true;
-
+        // 上層結果頁-按鈕
+        this.gameset.sprites.exit = new PIXI.Sprite(this.gameset.textures.exit);
+        this.gameset.sprites.exit.anchor.set(0.5);
+        this.gameset.sprites.exit.scale.set(this.gameset.size.w*0.05/14);
+        this.gameset.sprites.exit.x = this.gameset.size.w*0.95
+        this.gameset.sprites.exit.y = this.gameset.size.h*0.1
+        this.gameset.sprites.exit.interactive = true;
+        this.gameset.sprites.exit.buttonMode = true;
+        this.gameset.sprites.exit.on('pointerdown', this.closeCanvas.bind(this));
+        this.gameset.container.allStage.addChild(this.gameset.sprites.exit);
         this.gameset.bar_ticker.add(this.barLoop,this);
         this.gameset.bar_ticker.start();
         this.status = 'game'
@@ -232,7 +237,7 @@ export default class Game{
     }
     barLoop(){
         this.gameset.sprites.outerBar.x += this.gameset.sprites.outerBar.vx;
-        if (this.gameset.sprites.outerBar.x>200 || this.gameset.sprites.outerBar.x<0){
+        if (this.gameset.sprites.outerBar.x>this.gameset.sprites.innerBar.width || this.gameset.sprites.outerBar.x<0){
             this.gameset.sprites.outerBar.vx = -this.gameset.sprites.outerBar.vx
         }
     }
@@ -244,6 +249,7 @@ export default class Game{
         this.gameset.container.resultStage.removeChildren();
         this.gameset.container.gameStage.removeChildren();
         this.gameset.container.ruleStage.removeChildren();
+        this.gameset.container.bar.removeChildren();
         this.gameset.container.failStage.removeChildren();
         this.canvas.classList.add("d-none","loadering");
         if(this.status === 'success'){
@@ -254,19 +260,48 @@ export default class Game{
         }
     }
     shootBar(){
-        if (this.gameset.sprites.outerBar.x>33 && this.gameset.sprites.outerBar.x<110){
+        if (this.gameset.sprites.outerBar.x>this.gameset.sprites.pointBar.x && this.gameset.sprites.outerBar.x<this.gameset.sprites.pointBar.x+this.gameset.sprites.pointBar.width){
             this.gameset.bar_ticker.stop();
             this.gameset.container.resultStage.visible = true;
             this.gameset.sprites.sprite.interactive = false;
             const basicText = new PIXI.Text('成功',{
-                fontSize: 50
+                fontSize: 50,
+                fill: ['#ffffff']
             });
-            basicText.x = this.gameset.size.w / 2 - basicText.width / 2;
-            basicText.y = this.gameset.size.h / 2 - basicText.height / 2;
-            this.gameset.container.resultStage.addChild(basicText);
+            basicText.x = this.gameset.sprites.resultBg.width/2 - basicText.width/2;
+            basicText.y = this.gameset.sprites.resultBg.height/2 - basicText.height/2;
+            const video = document.createElement("video");
+            video.src = "image/video.mp4";
+            video.muted = true;
+            video.setAttribute("playsinline", "");
+            const sprite = PIXI.Sprite.from(video);
+            sprite.width = this.gameset.sprites.resultBg.width
+            sprite.height = this.gameset.sprites.resultBg.height
+            this.gameset.container.resultStage.addChild(sprite);
             this.status = 'success'
         }
-        else if(this.gameset.hearts>1){
+        else if(this.gameset.sprites.outerBar.x<this.gameset.sprites.pointBar.x && this.gameset.hearts>1){
+            this.gameset.container.failStage.removeChild(this.gameset.sprites.failText);
+            this.gameset.sprites.failText = new PIXI.Text('未命中，請再接再厲(失敗1)',{
+                fontSize: 50
+            })
+            this.gameset.sprites.failText.x = this.gameset.size.w / 2 - this.gameset.sprites.failText.width / 2;
+            this.gameset.sprites.failText.y = this.gameset.size.h*0.1;
+            this.gameset.container.failStage.addChild(this.gameset.sprites.failText);
+            this.gameset.container.failStage.visible = true;
+            this.gameset.sprites.sprite.interactive = false;
+            this.gameset.hearts--;
+            this.countHeart();
+            this.gameset.bar_ticker.stop();
+        }
+        else if(this.gameset.sprites.outerBar.x>this.gameset.sprites.pointBar.x+this.gameset.sprites.pointBar.width && this.gameset.hearts>1){
+            this.gameset.container.failStage.removeChild(this.gameset.sprites.failText);
+            this.gameset.sprites.failText = new PIXI.Text('未命中，請再接再厲(失敗2)',{
+                fontSize: 50
+            })
+            this.gameset.sprites.failText.x = this.gameset.sprites.resultBg.width/2 - this.gameset.sprites.failText.width / 2;
+            this.gameset.sprites.failText.y = this.gameset.sprites.resultBg.height*0.1;
+            this.gameset.container.failStage.addChild(this.gameset.sprites.failText);
             this.gameset.container.failStage.visible = true;
             this.gameset.sprites.sprite.interactive = false;
             this.gameset.hearts--;
@@ -278,10 +313,11 @@ export default class Game{
             this.gameset.container.resultStage.visible = true;
             this.gameset.sprites.sprite.interactive = false;
             const basicText = new PIXI.Text('失敗',{
-                fontSize: 50
+                fontSize: 50,
+                fill: ['#ffffff']
             });
-            basicText.x = this.gameset.size.w / 2 - basicText.width / 2;
-            basicText.y = this.gameset.size.h / 2 - basicText.height / 2;
+            basicText.x = this.gameset.sprites.resultBg.width/2 - basicText.width/2;
+            basicText.y = this.gameset.sprites.resultBg.height/2 - basicText.height/2;
             this.gameset.container.resultStage.addChild(basicText);
             this.status = 'fail'
         }
@@ -296,8 +332,7 @@ export default class Game{
         this.gameset.sprites.sprite.interactive = true;
     }
     countHeart(){
-        this.gameset.container.gameStage.removeChild(this.gameset.container.heartArea);
-        this.gameset.container.heartArea = new PIXI.Container();
+        this.gameset.container.heartArea.removeChildren();
         for (let i = 3; i > 0; i--){
             let heart = new PIXI.Sprite(this.gameset.textures.heart);
             heart.width = this.gameset.size.w*0.1;
@@ -312,30 +347,29 @@ export default class Game{
                 heart.mask = mask;
             }
             this.gameset.container.heartArea.addChild(heart);
-            this.gameset.container.gameStage.addChild(this.gameset.container.heartArea);
         }
     }
     resizeCanvas(){
         this.app.renderer.resize(window.innerWidth,window.innerHeight);
-        if(window.innerWidth > 960 && window.innerHeight > 720){
-            this.gameset.size.w = 960
+        if(window.innerWidth > 1280 && window.innerHeight > 720){
+            this.gameset.size.w = 1280
             this.gameset.size.h = 720
         }
-        else if(window.innerWidth < 960 && window.innerHeight > 720){
+        else if(window.innerWidth < 1280 && window.innerHeight > 720){
             this.gameset.size.w = window.innerWidth
-            this.gameset.size.h = window.innerWidth/4*3
+            this.gameset.size.h = window.innerWidth/16*9
         }
-        else if(window.innerWidth > 960 && window.innerHeight < 720){
-            this.gameset.size.w = window.innerHeight/3*4
+        else if(window.innerWidth > 1280 && window.innerHeight < 720){
+            this.gameset.size.w = window.innerHeight/9*16
             this.gameset.size.h = window.innerHeight
         }
-        else if(window.innerWidth < 960 && window.innerHeight < 720){
+        else if(window.innerWidth < 1280 && window.innerHeight < 720){
             if(window.innerWidth<window.innerHeight){
                 this.gameset.size.w = window.innerWidth
-                this.gameset.size.h = window.innerWidth/4*3
+                this.gameset.size.h = window.innerWidth/16*9
             }
             else{
-                this.gameset.size.w = window.innerHeight/3*4
+                this.gameset.size.w = window.innerHeight/9*16
                 this.gameset.size.h = window.innerHeight
             }
         }
@@ -345,7 +379,6 @@ export default class Game{
         this.gameset.container.allStage.width = this.gameset.size.w;
         this.gameset.container.allStage.height = this.gameset.size.h;
         this.gameset.container.allStage.position.set(window.innerWidth/2 - this.gameset.size.w/2,window.innerHeight/2 - this.gameset.size.h/2)
-        
     }
     onDragStart(event) {
         this.data = event.data;
@@ -361,16 +394,17 @@ export default class Game{
     
     onDragMove() {
         if (this.gameset.sprites.handle.dragging) {
+            
             const newPosition = this.gameset.sprites.handle.data.getLocalPosition(this.gameset.sprites.handle.parent);
-            if(newPosition.x<this.gameset.size.w/2*0.1){
-                newPosition.x = this.gameset.size.w*0.05
+            if(newPosition.x<this.gameset.sprites.slide.width/0.9*0.05){
+                newPosition.x = this.gameset.sprites.slide.width/0.9*0.05
             }
-            else if(newPosition.x>this.gameset.size.w*0.95){
-                newPosition.x = this.gameset.size.w*0.95;
+            else if(newPosition.x>this.gameset.sprites.slide.width/0.9*0.95){
+                // newPosition.x = this.gameset.sprites.slider.width/0.9*0.95;
                 this.gameset.container.startStage.visible = false;
             }
             this.gameset.sprites.handle.x = newPosition.x;
-            this.gameset.sprites.startBg.gotoAndStop(Math.floor(this.gameset.sprites.handle.x/this.gameset.size.w*0.95*10));
+            this.gameset.sprites.startBg.gotoAndStop(Math.floor(this.gameset.sprites.handle.x/this.gameset.sprites.slide.width*0.9*10));
         }
     }
 }
